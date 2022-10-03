aaa227cab31a0128cdceac0d310f074781b85881
ahmedsamy <e-a.sami@lean.sa>
2022-10-03 12:55:25 +0200

    Registering customer orders Creating order models

--- myshop/settings.py
+++ myshop/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
-    'cart.apps.CartConfig'
+    'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig'
 
 ]
 
